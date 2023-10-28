6632254c38f3448be944fff22c9dc333033f00ec
Damyenn Cardenas <damyenn3500@gmail.com>
2023-10-28 10:03:33 -0600

    Add Spark imports and create lines and links

--- src/main/java/IdealizedPageRank.java
+++ src/main/java/IdealizedPageRank.java
@@ -2,6 +2,17 @@ import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.avro.mapred.Pair;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import com.google.common.collect.Iterables;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.sql.SparkSession;
+
 public final class IdealizedPageRank {
     //groups with () are useful, i love them
     private static final Pattern links = Pattern.compile("^(\\d+):(.*)$");
@@ -20,5 +31,26 @@ public final class IdealizedPageRank {
             linkMap.put(linkMatcher.group(1), linkMatcher.group(2).split(" "));
         }
 
-    }
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaPageRank").master("local")
+                .getOrCreate();
+
+        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+
+        PairFunction<String, String, String> linkPair =
+                new PairFunction<String, String, String>() {
+                    @Override
+                    public Tuple2<String, String> call(String s) throws Exception {
+                        String[] tmp = s.split(":");
+                        return new Tuple2<>(tmp[0], tmp[1]);
+                    }
+                };
+
+        JavaPairRDD<String, String> linksRDD = lines.mapToPair(linkPair);
+
+        System.out.println("Links:");
+        linksRDD.take(4).forEach(System.out::println);
+        
+        }
 }
