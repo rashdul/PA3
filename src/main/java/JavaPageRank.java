a28ab8a4a77fa43f3aade936681573252e9e45d2
rashdul <123418201+rashdul@users.noreply.github.com>
2023-10-29 15:44:38 -0600

    Update JavaPageRank.java

--- src/main/java/JavaPageRank.java
+++ src/main/java/JavaPageRank.java
@@ -64,57 +64,47 @@ public final class JavaPageRank {
   }
 
   public static void main(String[] args) throws Exception {
-    if (args.length < 2) {
-      System.err.println("Usage: JavaPageRank <file> <number_of_iterations>");
-      System.exit(1);
+
+        if (args.length < 1) {
+            System.err.println("Usage: JavaWordCount <file>");
+            System.exit(1);
+        }
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaWordCount").master("local")
+                .getOrCreate();
+
+        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+        JavaPairRDD<String, String> links = lines.mapToPair(s -> new Tuple2<>(s.split(": ")[0], s.split(": ")[1]));
+        long nPage = links.count();
+        JavaPairRDD<String, Double>  ranks = links.mapValues(v1 -> 1.0/(double)nPage);
+
+
+        for (int i = 0; i < 25; i++) {
+            JavaPairRDD<String, Double> con = links.join(ranks).values().flatMapToPair(s -> {
+                String[] outgoingl = s._1().split(" ");
+                int len = outgoingl.length;
+                List<Tuple2<String, Double>> results = new ArrayList<>();
+                for (String n : outgoingl) {
+                    results.add(new Tuple2<>(n, s._2() / len));
+                }
+
+                return results.iterator();
+
+
+            });
+            ranks = con.reduceByKey((i1, i2) -> i1 + i2);
+        }
+
+
+
+        List<Tuple2<String, Double>> output = ranks.collect();
+        for (Tuple2<?, ?> tuple : output) {
+            System.out.println(tuple._1() + "\t" + tuple._2());
+        }
+        spark.stop();
     }
-
-    showWarning();
-
-    SparkSession spark = SparkSession
-      .builder()
-      .appName("JavaPageRank")
-      .getOrCreate();
-
-    // Loads in input file. It should be in format of:
-    //     URL         neighbor URL
-    //     URL         neighbor URL
-    //     URL         neighbor URL
-    //     ...
-    JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-
-    // Loads all URLs from input file and initialize their neighbors.
-    JavaPairRDD<String, Iterable<String>> links = lines.mapToPair(s -> {
-      String[] parts = SPACES.split(s);
-      return new Tuple2<>(parts[0], parts[1]);
-    }).distinct().groupByKey().cache();
-
-    // Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
-    JavaPairRDD<String, Double> ranks = links.mapValues(rs -> 1.0);
-
-    // Calculates and updates URL ranks continuously using PageRank algorithm.
-    for (int current = 0; current < Integer.parseInt(args[1]); current++) {
-      // Calculates URL contributions to the rank of other URLs.
-      JavaPairRDD<String, Double> contribs = links.join(ranks).values()
-        .flatMapToPair(s -> {
-          int urlCount = Iterables.size(s._1());
-          List<Tuple2<String, Double>> results = new ArrayList<>();
-          for (String n : s._1) {
-            results.add(new Tuple2<>(n, s._2() / urlCount));
-          }
-          return results.iterator();
-        });
-
-      // Re-calculates URL ranks based on neighbor contributions.
-      ranks = contribs.reduceByKey(new Sum()).mapValues(sum -> 0.15 + sum * 0.85);
-    }
-
-    // Collects all URL ranks and dump them to console.
-    List<Tuple2<String, Double>> output = ranks.collect();
-    for (Tuple2<?,?> tuple : output) {
-      System.out.println(tuple._1() + " has rank: " + tuple._2() + ".");
-    }
-
-    spark.stop();
-  }
 }
